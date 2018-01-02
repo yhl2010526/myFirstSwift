328052a6832264686c4ff170fb836a1939f7d584
Hellen <2564830681@qq.com>
2018-01-02 14:24:43 +0800

    change01

--- myFirstSwift/ViewController.swift
+++ myFirstSwift/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        first(_name: "hahaha")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func first(_name:Any) {
+        print("hello World ", _name)
+        
+        for x in 0...10 {
+            print("\(x) ")
+        }
+        print("---------------------------------------------")
+        var myString:String?
+        
+        myString = "Hello, Swift!"
+        
+        if myString != nil {
+            print(myString!)
+        }else{
+            print("myString 值为 nil")
+        }
+        print("---------------------------------------------")
+        let 你好 = "你好aa "
+        print(你好)
+        
+        //
+        for ch in "Runoob".utf8CString {
+            print(ch)
+        }
+        print("---------------------------------------------")
+        var v1:String = "jack"
+        let c1:Character = "G"
+        v1.append(c1)
+        print(v1)
+        
+        NSLog("nihao a wo shi oc")
+        
+        //数组
+        print("---------------------------------------------")
+        
+    }
+    
+    func second(){
+        print("add something")
+    }
 }
 
